cbde4bb034434c8b479ff1895d673111651515c0
Pavel Mitrofanov <195997+nblxa@users.noreply.github.com>
2020-01-26 10:06:34 +0100

    Use constants for messages and add javadoc for primitive-typed interfaces.

--- cons-list/src/main/java/io/github/nblxa/cons/DoubleConsList.java
+++ cons-list/src/main/java/io/github/nblxa/cons/DoubleConsList.java
@@ -4,6 +4,7 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.ThreadSafe;
+import java.util.NoSuchElementException;
 import java.util.PrimitiveIterator;
 import java.util.Spliterator;
 import java.util.stream.DoubleStream;
@@ -11,20 +12,55 @@ import java.util.stream.DoubleStream;
 @Immutable
 @ThreadSafe
 public interface DoubleConsList<E> extends ConsList<E> {
+
+    /**
+     * Returns the first element of the <tt>DoubleConsList</tt> as <tt>double</tt> primitive type.
+     *
+     * This method ensures no primitive boxing is taking place.
+     * <p>Throws {@link NoSuchElementException} if the list is empty.
+     *
+     * @return first element of the list
+     */
     double doubleHead();
 
+    /**
+     * Returns another <tt>DoubleConsList</tt> containing the current list's elements
+     * after the first one.
+     *
+     * <p>Throws {@link NoSuchElementException} if the list is empty.
+     *
+     * @return elements of the list after the first one
+     */
     @NonNull
     DoubleConsList<E> doubleTail();
 
+    /**
+     * Constructs a new <tt>DoubleConsList</tt> with the elements of the current one
+     * in the reverse order.
+     *
+     * @return new list with elements in reversed order
+     */
     @NonNull
     DoubleConsList<E> doubleReverse();
 
+    /**
+     * Creates a new primitive-typed iterator for this list of type <tt>double</tt>.
+     * @return new primitive-typed iterator
+     */
     @NonNull
     PrimitiveIterator.OfDouble doubleIterator();
 
+    /**
+     * Creates a new primitive-typed spliterator for this list of type <tt>double</tt>.
+     * @return new primitive-typed spliterator
+     */
     @NonNull
     Spliterator.OfDouble doubleSpliterator();
 
+    /**
+     * Creates a new primitive-typed stream from this list of type <tt>double</tt>.
+     * @return new primitive-typed stream
+     */
     @NonNull
     DoubleStream doubleStream();
 }
